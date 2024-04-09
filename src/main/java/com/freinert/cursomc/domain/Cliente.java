55d2e3c2bc45d6297f1b4ed4d3e4a20361be77c2
Fabio Reinert <fr2525@gmail.com>
2024-04-09 11:39:24 -0300

    Pedido, EstadoPagamento e Pagamento - parte 2

--- src/main/java/com/freinert/cursomc/domain/Cliente.java
+++ src/main/java/com/freinert/cursomc/domain/Cliente.java
@@ -39,6 +39,8 @@ public class Cliente  implements Serializable {
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	private List<Pedido> pedidos = new ArrayList<>();
+	
 	public Cliente() {
 		
 	}
@@ -88,23 +90,6 @@ public class Cliente  implements Serializable {
 		return TipoCliente.toEnum(tipo);
 	}
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Cliente other = (Cliente) obj;
-		return Objects.equals(id, other.id);
-	}
-
 	public void setTipo(TipoCliente tipo) {
 		this.tipo = tipo.getCod();
 	}
@@ -124,5 +109,24 @@ public class Cliente  implements Serializable {
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
 	}
+
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(id, other.id);
+	}
+
 	
 }
